9fe51f5202c8cc5e9bb26e3b4460888be939035c
liuhongwei.119 <769591656@qq.com>
2022-11-27 12:25:22 +0800

    test_app

--- app.py
+++ app.py
@@ -8,10 +8,17 @@ def hello_world():  # put application's code here
     return 'Hello World!'
 
 
+def create_app():
+    # 这个工厂方法可以从你的原有的 `__init__.py` 或者其它地方引入。
+    app = Flask(__name__)
+    return app
+
+
+application = create_app()
+
 if __name__ == '__main__':
-    app.run(
+    application.run(
         debug=True,
         port=8000,
         host="0.0.0.0"
     )
-
